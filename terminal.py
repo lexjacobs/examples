ea42ac342eedecfbf05f057df93991b9078c69a5
Alex Jacobs <lex.jacobs@gmail.com>
2018-05-19 16:41:15 -0700

    Revise std.py, reimplementation of original edits.

--- terminal.py
+++ terminal.py
@@ -6,34 +6,14 @@ ctx = Context('terminal', func=lambda app, win: any(
     t in app.bundle for t in terminals))
 
 keymap = {
-    'cd': 'cd ',
     'cd back': 'cd -; ls -a;\n',
     'cd develop': 'cd ~/develop; ls -a;\n',
     'cd home': 'cd ~; ls -a;\n',
     'cd parent': 'cd ..; ls -a;\n',
-    'run ellis': 'ls -a;\n',
-    'run make (durr | dear)': 'mkdir ',
+
     'snipple': [Key('ctrl-a'), Key('ctrl-k')],
+    'kite': [Key('esc'), Key('d')],
     'trough': [Key('ctrl-w')],
-
-    'run git': 'git ',
-    'run git clone': 'git clone ',
-    'run git checkout': 'git checkout ',
-    'run git diff': 'git diff \n',
-    'run git initialize': 'git init \n',
-    'run git commit': 'git commit ',
-    'run git log': 'git log \n',
-    'run git push': 'git push ',
-    'run git push origin': 'git push origin ',
-    'run git push origin master': 'git push origin master ',
-    'run git pull': 'git pull ',
-    'run git reset': 'git reset ',
-    'run git status': 'git status \n',
-    'run git tag': 'git tag ',
-    'run git add': 'git add ',
-    'run git add dot': 'git add .\n',
-    'run git amend': 'git add .; git commit --amend --no-edit\n',
-
     'tools oedipus': [Key('ctrl-x'), Key('ctrl-e')],
     'open sublime': Str('subl .\n'),
     'open adam': Str('atom .\n'),
