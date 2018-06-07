adfc5f91b3c80f7575e0cf0116d833d65b0f52a2
Alex Jacobs <lex.jacobs@gmail.com>
2018-06-07 12:03:45 -0700

    Add shrink map

--- words.py
+++ words.py
@@ -1,14 +1,14 @@
-from talon.voice import Context, Key
+from talon.voice import Context, Key, Str
 
 ctx = Context('words')
+def shrink_word(m):
+    word = str(m.dgndictation[0]._words[0]).lower()
+    if not word in shrink_map:
+        raise Exception('%s not in shrink map' % word)
+    Str(shrink_map[word])(None)
 
 keymap = {
     'dot company': '.com',
-
-    'state command': 'cmd',
-    'state control': 'ctrl',
-    'state option': 'opt',
-    'state static': 'static ',
     'word parse integer': ['parseInt()', Key('left')],
 
     'word and': 'end',
@@ -27,9 +27,41 @@ keymap = {
     'word printf': 'printf',
     'word slack': 'slack',
     'word string': 'JSON.stringify',
+    'word pseudo-\\\\pseudo': 'sudo ',
     'word them': 'vim',
     'word will': 'twilio',
     'word with': 'width',
+    'shrink <dgndictation>': shrink_word,
+}
+
+shrink_map = {
+
+    'administrator': 'admin',
+    'alternate': 'alt',
+    'apartment': 'apt',
+    'applications': 'apps',
+    'arguments': 'args',
+    'attributes': 'attrs',
+    'button': 'btn',
+    'command': 'cmd',
+    'compute': 'cmp',
+    'context': 'ctx',
+    'concatenate': 'concat',
+    'configure': 'config',
+    'control': 'ctrl',
+    'format': 'fmt',
+    'image': 'img',
+    'jason': 'json',
+    'message': 'msg',
+    'package': 'pkg',
+    'parameter': 'param',
+    'source': 'src',
+    'standard': 'std',
+    'temporary': 'tmp',
+    'text': 'txt',
+    'user': 'usr',
+    'user id': 'uid',
+    'utilities': 'utils',
 }
 
 ctx.keymap(keymap)
