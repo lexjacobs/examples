f04ee1969180f6f65c9eb16488d2baaf128a5399
Alex Jacobs <lex.jacobs@gmail.com>
2018-11-09 12:29:13 -0800

    add additional websites

--- websites.py
+++ websites.py
@@ -4,13 +4,18 @@ import os
 
 ctx = Context('websites')
 
+
 def Paste(s):
     def func(m):
         clip.set(s)
         press('cmd-v')
     return func
 
+
 ctx.keymap({
+    'website doc': lambda m: os.system('open https://duckduckgo.com/'),
+    'website get hub': lambda m: os.system('open https://github.com/lexjacobs/'),
     'website gmail': lambda m: os.system('open https://gmail.com'),
-    'website get hub': lambda m: os.system('open https://github.com/lexjacobs/')
+    'website google': lambda m: os.system('open https://google.com'),
+    'website stack': lambda m: os.system('open https://stackoverflow.com/'),
 })
