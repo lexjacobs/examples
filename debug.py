2ddc20f5004ae43951f81a0d78f729b5affa76e9
Alex Jacobs <lex.jacobs@gmail.com>
2019-07-17 14:25:10 -0700

    add back old debug

---
+++ debug.py
@@ -0,0 +1,13 @@
+from talon.engine import engine
+
+
+def listener(topic, m):
+    if topic == "cmd" and m["cmd"]["cmd"] == "g.load" and m["success"] == True:
+        print("[grammar reloaded]")
+    if topic == "cmd":
+        return
+    else:
+        print(topic, m)
+
+
+engine.register("", listener)
